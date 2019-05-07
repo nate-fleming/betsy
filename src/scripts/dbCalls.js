35f8a2290198dca1f76aec4f35b9b08f56890521
nate-fleming <nfleming4@gmail.com>
2019-05-07 14:49:55 -0500

    fix grunt api server

--- src/scripts/dbCalls.js
+++ src/scripts/dbCalls.js
@@ -1,14 +1,14 @@
 const callDB = {
     getProducts() {
-        return fetch(" http://localhost:3000/products")
+        return fetch(" http://localhost:8088/products")
             .then(entries => entries.json())
     },
     getOneProduct(id) {
-        return fetch(`http://localhost:3000/products/${id}`)
+        return fetch(`http://localhost:8088/products/${id}`)
             .then(entries => entries.json())
     },
     getProductReviews(id) {
-        return fetch(`http://localhost:3000/products/${id}?_embed=reviews`)
+        return fetch(`http://localhost:8088/products/${id}?_embed=reviews`)
             .then(entries => entries.json())
     }
 }
